d95cfa7cfceb982b0819666254c055206699d9c0
jesstanblues-ops <jesstanblues@gmail.com>
2025-11-18 11:06:19 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ SYSTEM_START_YEAR = 2026
 EMPLOYEES = [
     {"name": "YONG YUN CHIN", "role": "Staff", "join_date": "2008-08-08", "entitlement": 18},
     {"name": "JUBILIN MORIS", "role": "Staff", "join_date": "2012-01-01", "entitlement": 18},
-    {"name": "ABDULLAH BIN ALADDIN", "role": "Staff", "join_date": "2012-02-01", "entitlement": 16},
+    {"name": "ABDULLAH BIN ALADIN", "role": "Staff", "join_date": "2012-02-01", "entitlement": 16},
     {"name": "MAT SAHAK BIN ABDULLAH", "role": "Staff", "join_date": "2015-02-02", "entitlement": 16},
     {"name": "VYANESSA STEVEN WONG", "role": "Staff", "join_date": "2018-03-01", "entitlement": 16},
     {"name": "MARY IMMACULATE", "role": "Staff", "join_date": "2021-02-01", "entitlement": 16},
